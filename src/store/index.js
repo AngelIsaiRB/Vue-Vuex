539c08cd5560252fb6b6970b462b5e4905d99228
Anegelisai <63125934+Anegelisai@users.noreply.github.com>
2021-01-15 15:30:07 -0600

    vuex store-mutations & actions

--- src/store/index.js
+++ src/store/index.js
@@ -6,8 +6,21 @@ export default createStore({
 
   },
   mutations: {
+    incrementar(state){
+      state.contador += 10 ;
+    },
+    disminuir(state, payload){
+      state.contador -= payload ;
+    }
+
   },
   actions: {
+    accionIncrementar({commit}){
+      commit("incrementar")
+    },
+    accionDisminuir({commit}, numero ){
+      commit("disminuir", numero)
+    },
   },
   modules: {
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,22 +3,31 @@
     <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
     <h1
       :style="colorContador"
-    >{{titulo}} {{contador}}</h1>
+    >{{titulo}} {{contador}}
+    </h1>    
+    <button @click="accionIncrementar">Incrementar</button>
+    <BtnDisminuir/>
   </div>
 </template>
 
 <script>
-import {mapState} from "vuex"
+import {mapState, mapMutations, mapActions} from "vuex"
+import BtnDisminuir from '../components/BtnDisminuir.vue'
+
 
 export default {
   name: 'Home',
   components: {
-  
+    BtnDisminuir,
   },
   data() {
     return {
       titulo: "mi contador:"
     }
+  },
+  methods: {
+    // ...mapMutations(["incrementar"]),
+    ...mapActions(["accionIncrementar"])
   },
   computed:{
     ...mapState(["contador"]),
